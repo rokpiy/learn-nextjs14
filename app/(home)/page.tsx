2af1ba1c188cb0bb92253651262038b04f88cac5
rokpiy <inyoung@aristo.so>
2025-10-21 20:19:30 +0900

    Basic Setups for pages

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,25 @@
-export const metadata = {
-    title: 'Home',
-  }
+import Link from "next/link";
 
-export default function Tomato(){
-    return (
-    <div>Tomato
-    </div>
-    )
+export const metadata = {
+  title: 'Home',
+};
+
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies(){
+  //wait new Promise(resolve => setTimeout(resolve, 1000));
+  const response = await fetch(API_URL);
+  const json = await response.json();
+  return json;
 }
+
+export default async function HomePage(){
+  const movies = await getMovies();
+  return (
+      <div> {movies.map((movie) => 
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      )}</div>
+    )
+  }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,3 +1,19 @@
-export default function MoviePage({ params }: { params: { id: string } }) {
-    return <div>Movie {params.id}</div>
+import { API_URL } from "../../../(home)/page";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
+import { Suspense } from "react";
+
+export default async function MovieDetail({ params: {id}}: { params: { id: string } }) {
+    
+    return (
+              <div>
+            <h3>Movie detail page</h3>
+            <Suspense fallback={<div>Loading movie info...</div>}>
+                <MovieInfo id={id} />
+            </Suspense>
+            <Suspense fallback={<div>Loading movie videos...</div>}>
+                <MovieVideos id={id} />
+            </Suspense>
+        </div>
+    )
 }
